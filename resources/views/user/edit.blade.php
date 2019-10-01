b465e45f4c0481d30833845d2142bda6e41eb856
Maulana Kevin Pradana <44237629+maulanakevinpradana@users.noreply.github.com>
2019-10-01 22:46:10 +0700

    update

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -12,6 +12,15 @@
             <li class="breadcrumb-item active" aria-current="page">{{ $subtitle }}</li>
         </ol>
     </nav>
+    @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
     @if (session('success'))
     <div class="alert alert-success">
         {{ session('success') }}
